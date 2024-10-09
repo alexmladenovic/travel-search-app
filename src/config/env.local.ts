3637c8d1cdf4b55bdd9b66cb40ba3a54d0e44726
Aleksandar Mladenovic <alex.mladenovic2000@gmail.com>
2024-10-09 13:06:17 +0300

    Add Github actions + environment variables

---
+++ src/config/env.local.ts
@@ -0,0 +1,6 @@
+
+export default {
+  APP_URL: 'http://localhost:3000',
+  LANDING_URL: 'http://localhost:3002',
+  API_URL: 'http://localhost:3005',
+}
---
+++ src/config/env.ts
@@ -0,0 +1,16 @@
+import local from './env.local'
+import production from './env.production'
+
+let env = local
+switch (process.env.REACT_APP_ENV) {
+  case 'local':
+    env = local
+    break
+  case 'production':
+    env = production
+    break
+  default:
+    env = local
+}
+
+export default env
